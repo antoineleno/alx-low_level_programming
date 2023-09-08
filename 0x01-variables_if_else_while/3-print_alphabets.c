9aa830772df7233dac7f9db3c78ee87ee7c5a22a
antoineleno <141551233+antoineleno@users.noreply.github.com>
2023-09-09 00:20:04 +0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
-* main - Prints the alphabet.
+* main - Prints the alphabet without q and e.
 *
 * Return: Always 0 (Success)
 */
 int main(void)
+{int i;
+for (i = 97; i < 123; i++)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-int i;
-for (i = 0; i < 52; i++)
+if (i != 101 && i != 113)
 {
-putchar(alp[i]);
+putchar(i);
+}
 }
 putchar('\n');
 return (0);
