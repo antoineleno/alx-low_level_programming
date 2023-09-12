f6bfd527ae0bfbb0cacb3d0a247f22386925b32a
antoineleno <141551233+antoineleno@users.noreply.github.com>
2023-09-12 18:49:08 +0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,7 +15,7 @@ for (j = 0; j <= n; j++)
 k = j * i; if (j == 0)
 {
 _putchar(k + '0');
-} 
+}
 else if (k < 10 && j != 0)
 {
 _putchar(',');
@@ -23,7 +23,7 @@ _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} 
+}
 else if (k >= 10 && k < 100)
 {
 _putchar(',');
@@ -31,7 +31,7 @@ _putchar(' ');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
-} 
+}
 else if (k >= 100)
 {
 _putchar(',');
