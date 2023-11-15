cb40e0b0354c14c837750365e6644acb8a6642de
antoineleno <johndoe@example.com>
2023-11-15 18:18:01 +0800

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - function like puts();
+ *@n: input
+ *Return: Always 0 (Sucess)
+*/
+
+
+void _puts_recursion( char *n)
+{
+    if (*n)
+    {
+        _putchar(*n);
+        _puts_recursion(n + 1);
+    }
+    else
+    {
+        _putchar('\n');
+    }
+}
