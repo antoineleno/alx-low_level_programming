57ac5fd2d971b5b6caa17d04412e52f5d7218fd9
antoineleno <johndoe@example.com>
2024-01-16 01:34:38 +0800

    Malloc and free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,16 @@ char *str_concat(char *s1, char *s2)
 	char *buffer;
 	unsigned int i, j;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 == "";
+	}
+
 	length_of_s1 = _strlen(s1);
 	length_of_s2 = _strlen(s2);
 
