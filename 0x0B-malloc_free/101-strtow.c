1127bab6451f1d9526b5e457cb9aeaf7cb3237c4
antoineleno <johndoe@example.com>
2024-01-16 12:47:34 +0800

    Malloc and free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -15,3 +15,37 @@ char **strtow(char *str)
 	
 	
 }
+
+
+/**
+ * word_count - Function to count the number of words in the string.
+ * @string: Argument of the function
+ * Return: The number of words
+*/
+
+int word_count(char *string)
+{
+	int count = 0;
+	
+	if (*string == '\t' || *string == ' ' || *string == '\n')
+	{
+		string++;
+	}
+
+	while (*string != '\0')
+	{
+		if (*string == '\t' || *string == ' ' || *string == '\n')
+		{
+			count++;
+			while (*string == '\t' || *string == ' ' || *string == '\n')
+			{
+				string++;
+			}
+		}
+		else
+		{
+			string++;
+		}
+	}
+	return (count);
+}
