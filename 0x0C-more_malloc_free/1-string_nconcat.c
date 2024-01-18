1b133f89514aae060e00cc0f444cef977107537f
antoineleno <johndoe@example.com>
2024-01-19 05:34:46 +0800

    More malloc and free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,49 @@
-#include <stdlib.h>
-#include "main.h"
-
+#include "main.h" 
 /**
- * *string_nconcat - concatenates n bytes of a string to another string
- * @s1: string to append to
- * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
- *
- * Return: pointer to the resulting string
+ * string_nconcat - Function to concatenate n characters from s2 to s1.
+ * @s1: First string
+ * @s2: Second string
+ * @n: Number of characters from s2 to concatenate
+ * Return: Concatenated string, or NULL on failure
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int i = 0, j = 0, length1 = 0, length2 = 0;
+    char *dest_string;
+    unsigned int i = 0, j = 0, length1 = 0, length2 = 0;
 
-	while (s1 && s1[length1])
-		length1++;
-	while (s2 && s2[length2])
-		length2++;
-
-	if (n < length2)
-		s = malloc(sizeof(char) * (length1 + n + 1));
-	else
-		s = malloc(sizeof(char) * (length1 + length2 + 1));
-
-	if (!s)
-		return (NULL);
-
-	while (i < length1)
-	{
-		s[i] = s1[i];
-		i++;
-	}
-
-	while (n < length2 && i < (length1 + n))
-		s[i++] = s2[j++];
-
-	while (n >= length2 && i < (length1 + length2))
-		s[i++] = s2[j++];
-
-	s[i] = '\0';
-
-	return (s);
+    while (*s1 != '\0')
+    {
+        length1++;
+    }
+    while (*s2 != '\0')
+    {
+        length2++;
+    }
+    if (n < length2)
+    {
+        dest_string = malloc((length1 + n + 1) * sizeof(char));
+    }
+    else
+    {
+        dest_string = malloc((length1 + length2 + 1) * sizeof(char));
+    }
+    if (dest_string == NULL)
+    {
+        return (NULL);
+    }
+    while (i < length1)
+    {
+        dest_string[i] = s1[i];
+        i++;
+    }
+    while (n < length2 && i < (length1 + n))
+    {
+        dest_string[i++] = s2[j++]; 
+    }
+    while (n >= length2 && i < (length1 + length2))
+    {
+        dest_string[i++] = s2[j++];
+    }
+    dest_string[i] = '\0';
+    return (dest_string);
 }
