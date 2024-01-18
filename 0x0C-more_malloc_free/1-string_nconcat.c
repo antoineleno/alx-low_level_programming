5003d76747576e46c92c642f5dd3f29a88aac497
antoineleno <johndoe@example.com>
2024-01-19 05:55:45 +0800

    More malloc and free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include "main.h"
 /**
  * string_nconcat - Function to concatenate n characters from s2 to s1.
  * @s1: First string
@@ -8,42 +8,43 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *dest_string;
-    unsigned int i = 0, j = 0, length1 = 0, length2 = 0;
 
-    while (s1 && s1[length1])
-    {
-        length1++;
-    }
-    while (s2 && s2[length2])
-    {
-        length2++;
-    }
-    if (n < length2)
-    {
-        dest_string = malloc((length1 + n + 1) * sizeof(char));
-    }
-    else
-    {
-        dest_string = malloc((length1 + length2 + 1) * sizeof(char));
-    }
-    if (dest_string == NULL)
-    {
-        return (NULL);
-    }
-    while (i < length1)
-    {
-        dest_string[i] = s1[i];
-        i++;
-    }
-    while (n < length2 && i < (length1 + n))
-    {
-        dest_string[i++] = s2[j++]; 
-    }
-    while (n >= length2 && i < (length1 + length2))
-    {
-        dest_string[i++] = s2[j++];
-    }
-    dest_string[i] = '\0';
-    return (dest_string);
+char *dest_string;
+unsigned int i = 0, j = 0, length1 = 0, length2 = 0;
+
+while (s1 && s1[length1])
+{
+length1++;
+}
+while (s2 && s2[length2])
+{
+length2++;
+}
+if (n < length2)
+{
+dest_string = malloc((length1 + n + 1) * sizeof(char));
+}
+else
+{
+dest_string = malloc((length1 + length2 + 1) * sizeof(char));
+}
+if (dest_string == NULL)
+{
+return (NULL);
+}
+while (i < length1)
+{
+dest_string[i] = s1[i];
+i++;
+}
+while (n < length2 && i < (length1 + n))
+{
+dest_string[i++] = s2[j++];
+}
+while (n >= length2 && i < (length1 + length2))
+{
+dest_string[i++] = s2[j++];
+}
+dest_string[i] = '\0';
+return (dest_string);
 }
