b6043509ae269d547151440e14a367f771d37875
antoineleno <johndoe@example.com>
2024-01-19 04:59:55 +0800

    More malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,41 @@
-#include "main.h"
+#include "main.h" 
+
 
 /**
- * string_nconcat - Function to concatenate n character
+ * string_nconcat - Function to concatenate n characters from s2 to s1.
  * @s1: First string
  * @s2: Second string
- * @n: The number of character to be concatenate
- * Return: concatenated string.
-*/
+ * @n: Number of characters from s2 to concatenate
+ * Return: Concatenated string, or NULL on failure
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int lenght1, lenght2, i, j;
-	char *dest_string;
+    unsigned int len1, len2, i, j;
+    char *result;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		lenght1 = _strlen(s1);
-		lenght2 = _strlen(s2);
-	}
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
 
-	if (n >= lenght2)
-	{
-		n = lenght2;
-	}
-	dest_string = malloc(lenght1 + n + 1);
-	if (dest_string == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < lenght1; i++)
-	{
-		dest_string[i] = s1[i];
-	}
-	for (j = 0; j < n; j++)
-	{
-		dest_string[i++] = s2[j];
-	}
-	dest_string[i++] = '\0';
+    len1 = _strlen(s1);
+    len2 = _strlen(s2);
 
+    if (n >= len2)
+        n = len2;
 
-	return (dest_string);
+    result = malloc(len1 + n + 1);  // +1 for null terminator
+
+    if (result == NULL)
+        return NULL;
+
+    for (i = 0; i < len1; i++)
+        result[i] = s1[i];
+
+    for (j = 0; j < n; j++)
+        result[i++] = s2[j];
+
+    result[i] = '\0';
+
+    return result;
 }
