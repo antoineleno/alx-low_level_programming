899187f4a769d3fd023b7e5ed28659b6b749e5b7
antoineleno <johndoe@example.com>
2024-01-20 15:33:03 +0800

    More macro and free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * _realloc - Function to realloc memory after allocating it with mallocc
+ * @old_size: First size while using malloc
+ * @new_size: Second size to realloc memory
+ * Return: No value to be return.
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+		{
+			return (NULL);
+		}
+		return (new_ptr);
+	}
+
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	new_ptr = malloc(new_size);
+	{
+		if (new_ptr == NULL)
+		{
+			return (NULL);
+		}
+	}
+
+	if (old_size < new_size)
+	{
+		memcpy(new_ptr, ptr, old_size );
+	}
+
+	else
+	{
+		memcpy(new_ptr, ptr, new_size);
+	}
+
+
+
+	free(ptr);
+	return (new_ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <string.h>
 
 void *malloc_checked(unsigned int b);
 unsigned int _strlen(char *string);
@@ -10,5 +11,6 @@ int _putchar(char c);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 char *_memset(char *s, char b, unsigned int n);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
