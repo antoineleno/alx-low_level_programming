3b6cf965fc3aab1d3bbe4bb3426179ac5782fe72
antoineleno <johndoe@example.com>
2024-01-20 17:03:56 +0800

    More macro and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -39,7 +39,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			new_ptr[i] = old_ptr[i];
 		}
 	}
-	if (new_size < old_size)
+	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
 		{
