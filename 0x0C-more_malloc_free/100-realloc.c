b04e9a747dbe9ce697e1ffe27013362abe989fce
antoineleno <johndoe@example.com>
2024-01-20 16:55:01 +0800

    More macro and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
  * _realloc - Function to realloc memory after allocating it with mallocc
  * @old_size: First size while using malloc
  * @new_size: Second size to realloc memory
- * Return: No value to be return.
+ * @ptr: pointer to the allocated memory
+ * Return: The new_allocated
 */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *new_ptr;
@@ -14,16 +13,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (old_size == new_size)
-	{
 		return (ptr);
-	}
-
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	if (ptr == NULL)
 	{
 		new_ptr = malloc(new_size);
@@ -33,15 +28,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		}
 		return (new_ptr);
 	}
-
-
-
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 	{
 		return (NULL);
 	}
-
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
@@ -49,9 +40,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			new_ptr[i] = old_ptr[i];
 		}
 	}
-
 	old_ptr = ptr;
-
 	if (new_size < old_size)
 	{
 		for (i = 0; i < old_size; i++)
