9fef96cfcf75874bacf7f85a599d6e39dfd45fa7
antoineleno <johndoe@example.com>
2024-01-23 01:53:35 +0800

    structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -28,3 +28,44 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog_ptr->age  = age;
 	return (new_dog_ptr);
 }
+
+
+/**
+ * _strdup - Function to duplicate a string?
+ * @str: String to be duplicate
+ * Return: The duplicate string.
+*/
+
+char *_strdup(char *str)
+{
+	unsigned int length_of_str = 0, i;
+	char *new_str = str;
+	char *buffer;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (*new_str != '\0')
+	{
+		length_of_str++;
+		new_str++;
+	}
+
+
+	buffer = (char *)malloc((length_of_str + 1) * sizeof(char));
+
+	if (buffer == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < length_of_str; i++)
+	{
+		buffer[i] = str[i];
+	}
+
+	buffer[i] = '\0';
+
+	return (buffer);
+}
