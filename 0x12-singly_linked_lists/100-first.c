b130dac4b64bc056087d3084dd41be4e23689415
antoineleno <johndoe@example.com>
2024-02-02 01:28:41 +0800

    printf

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+void first(void) __attribute__ ((constructor));
+
 /**
- * My_starting_message - Function to be execute before the main function
-*/
+ * My_starting_message - prints a sentence before the main
+ * function is executed
+ */
 
 
 void My_starting_message(void)
