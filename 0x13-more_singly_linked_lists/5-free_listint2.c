d497672fc35600a0d86d5607a52b45d397871fcc
antoineleno <johndoe@example.com>
2024-02-06 18:00:09 +0800

    d

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,14 +8,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 	{
 		free(head);
-	}
-
-	if (*head == NULL)
-	{
-		free(*head);
+		return;
 	}
 
 	while (*head != NULL)
