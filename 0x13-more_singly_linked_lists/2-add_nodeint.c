854389ca0b9f6551aa9c6a996a6eb0b12d00be36
antoineleno <johndoe@example.com>
2024-02-06 15:42:18 +0800

    d

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,6 +12,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new_node;
 
 	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+	{
+		return (NULL);
+	}
 
 
 	new_node->n = n;
@@ -20,12 +24,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	{
 		new_node = *head;
 		return (new_node);
-	}
-
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
 	}
 
 	new_node->next = *head;
