8c8343ca94318f962c9b8f344c480a8993b58cd8
antoineleno <johndoe@example.com>
2024-02-06 18:13:21 +0800

    i

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,7 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 	}
 
-	if (current == NULL)
+	if (i != idx - 1 || current == NULL)
 	{
 		free(new_node);
 		return (NULL);
