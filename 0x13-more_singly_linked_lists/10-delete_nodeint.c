8f16eae1b9ff468090356d389beace3cfc98e388
antoineleno <johndoe@example.com>
2024-02-06 16:46:11 +0800

    p

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = (*head)->next;
 		free(current);
-		return (-1);
+		return (1);
 	}
 
 	for (i = 0; i < index - 1 && current != NULL; i++)
