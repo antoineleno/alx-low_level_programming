ca7e0a76cc184635df983812d3395b3c3f037f6d
antoineleno <johndoe@example.com>
2024-02-06 18:33:58 +0800

    MOre singldy list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * reverse_listint - Function to reserve a list
+ * @head: head of the list
+ * Return: The reverse list
+*/
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+	return (prev);
+}
