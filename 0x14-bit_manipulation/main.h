6c9aa1898af2f2c389f0b6378301dfdf3842318d
antoineleno <johndoe@example.com>
2024-02-09 01:25:12 +0800

    Bit manupilation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,7 +4,6 @@
 #include <stddef.h>
 
 int _putchar(char c);
-int _strlen(const char *string);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
