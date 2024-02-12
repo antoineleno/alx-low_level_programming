a456e48e2273b44427e277aab5fe6f0610c4d5fe
antoineleno <johndoe@example.com>
2024-02-13 03:13:21 +0800

    File

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,15 +18,13 @@ int main(int argc, char *argv[])
 	source_file = open(argv[1], O_RDONLY);
 	if (source_file == -1 || argv[1] == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error : Can't read from %s\n", argv[1]);
-		close(source_file);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
 		exit(98);
 	}
 	destination_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (destination_file == -1)
 	{
-		dprintf(STDERR_FILENO, "Error : Can't write to %s\n", argv[2]);
-		close(destination_file);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	while ((bytes_read = read(source_file, buffer, sizeof(buffer) - 1)) > 0)
@@ -34,13 +32,13 @@ int main(int argc, char *argv[])
 		bytes_written = write(destination_file, buffer, bytes_read);
 		if (bytes_written == -1)
 		{
-			dprintf(STDERR_FILENO, "Error : Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
 	if (close(source_file) == -1 || close(destination_file) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error : Can't close %d\n", (source_file == -1) ?
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", (source_file == -1) ?
 		source_file : destination_file);
 		exit(100);
 	}
