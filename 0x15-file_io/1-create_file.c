525240c7c99ba08f934ad3890be31f16de896aef
antoineleno <johndoe@example.com>
2024-02-13 02:06:30 +0800

    File

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,25 +8,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-int fd, w, len = 0;
+	int dp;
+	int bytes_written, bytes_read;
 
 	if (filename == NULL)
-		return (-1);
-
-	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		return (-1);
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	bytes_read = strlen(text_content);
 
-	if (fd == -1 || w == -1)
+
+	dp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	bytes_written = write(dp, text_content, bytes_read);
+
+	if (dp == -1 || bytes_written == -1)
+	{
 		return (-1);
+	}
 
-	close(fd);
-
+	close(dp);
 	return (1);
-
 }
