d99ddb9da4836334525ca6efc1950272c2b57fbf
antoineleno <johndoe@example.com>
2024-02-13 02:01:48 +0800

    File

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,37 +8,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int dp;
-	char message[6] = "fails";
-	int bytes_written, bytes_read;
+int fd, w, len = 0;
 
-
-	dp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-	if (dp == -1)
-	{
-		write(STDOUT_FILENO, message, strlen(message));
-		close(dp);
+	if (filename == NULL)
 		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	bytes_read = strlen(text_content);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-	bytes_written = write(dp, text_content, bytes_read);
-
-	if (bytes_written == -1)
-	{
-		write(STDOUT_FILENO, message, strlen(message));
-		close(dp);
+	if (fd == -1 || w == -1)
 		return (-1);
-	}
 
-	if (bytes_read != bytes_written)
-	{
-		close(dp);
-		return (-1);
-	}
+	close(fd);
 
-	close(dp);
 	return (1);
+
 }
