ae6e4523db5d6e3a4cec05ad409f3bb95a889dba
antoineleno <johndoe@example.com>
2024-02-13 01:31:59 +0800

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,6 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int dp;
+	char message[5] = "fails";
 	int bytes_written, bytes_read;
 
 	if (filename == NULL)
@@ -25,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (dp == -1)
 	{
-		write(STDOUT_FILENO, "fails", 5);
+		write(STDOUT_FILENO, message, strlen(message));
 		close(dp);
 		return (-1);
 	}
