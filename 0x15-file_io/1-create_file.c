db74474c6358c0ba72203db2428a2f8009f602ba
antoineleno <johndoe@example.com>
2024-02-13 01:36:15 +0800

    File

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	dp = open(filename, O_WRONLY | O_CREAT, 0600);
+	dp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (dp == -1)
 	{
