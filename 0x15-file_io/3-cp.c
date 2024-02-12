982e77928e5634d510ad1d3258f54c29baf7b6c2
antoineleno <johndoe@example.com>
2024-02-13 03:10:06 +0800

    file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,12 +19,14 @@ int main(int argc, char *argv[])
 	if (source_file == -1 || argv[1] == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error : Can't read from %s\n", argv[1]);
+		close(source_file);
 		exit(98);
 	}
 	destination_file = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (destination_file == -1)
 	{
 		dprintf(STDERR_FILENO, "Error : Can't write to %s\n", argv[2]);
+		close(destination_file);
 		exit(99);
 	}
 	while ((bytes_read = read(source_file, buffer, sizeof(buffer) - 1)) > 0)
