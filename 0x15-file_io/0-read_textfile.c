fa03034c7ad5cdeffa1de45199f4c42262dd4531
antoineleno <johndoe@example.com>
2024-02-13 00:51:14 +0800

    File

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -30,22 +30,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(dp);
 		return (0);
 	}
-
 	bytes_read = read(dp, buffer, letters);
-	if (bytes_read == - 1)
+	if (bytes_read == -1)
 	{
 		close(dp);
 		return (0);
 	}
-
 	bytes_written = write(1, buffer, bytes_read);
-	if (bytes_written == - 1 || bytes_written != bytes_read)
+	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
 		close(dp);
 		return (0);
 	}
-
-
 	close(dp);
 	free(buffer);
 	return (bytes_written);
