281de825984a3179362b97c66efeb962217b2875
antoineleno <johndoe@example.com>
2024-02-13 03:05:12 +0800

    File

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,8 +8,8 @@
 int main(int argc, char *argv[])
 {
 	int destination_file, source_file, bytes_read, bytes_written;
-	char *buffer;
-	buffer = malloc(1025 * sizeof(char));
+	char buffer[1025];
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
@@ -44,6 +44,5 @@ int main(int argc, char *argv[])
 	}
 	close(source_file);
 	close(destination_file);
-	free(buffer);
 	return (0);
 }
