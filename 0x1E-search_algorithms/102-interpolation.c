e7ea275d4ac58fabfe80e0e1bc776ee15f41c0a4
antoineleno <johndoe@example.com>
2024-07-16 22:05:31 +0800

    search algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -46,7 +46,7 @@ int interpolation_search(int *array, size_t size, int value)
 	}
 
 
-	printf("Value checked array[%ld] is out of range\n", pos);
+	printf("Value checked array[%ld] is out of range\n", array[pos]);
 	return (-1);
 }
 
