898e4a3fd5572af8ea9fb57e872843a3a968798b
antoineleno <johndoe@example.com>
2024-07-16 21:34:59 +0800

    search algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,8 +11,39 @@
 
 int interpolation_search(int *array, size_t size, int value)
 {
+        size_t low, high, pos;
+
+        low = 0;
+        high = size - 1;
+        
+
+
         if (array == NULL)
         {
                 return (-1);
         }
+
+        while (low <= high && value >= array[low] && value <= array[high])
+        {
+                pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+                printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+
+                if (array[pos] == value)
+                {
+                        return (pos);
+                }
+
+                if (array[pos] > value)
+                {
+                        high = pos - 1;
+                }
+                else
+                {
+                        low = pos + 1;
+                }
+        }
+
+
+        printf("Value checked array[%ld] is out of range\n", pos);
+        return (-1);
 }
