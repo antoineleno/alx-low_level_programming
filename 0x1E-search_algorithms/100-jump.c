3d0c1831281b34d2d6e390bd5662960958a47edb
antoineleno <johndoe@example.com>
2024-07-16 19:23:45 +0800

    search algorithms

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,41 +12,41 @@
 
 int jump_search(int *array, size_t size, int value)
 {
-    size_t low, high, my_jump, i;
+	size_t low, high, my_jump, i;
 
-    if (array == NULL || size == 0)
-    {
-        return (-1);
-    }
+	if (array == NULL || size == 0)
+	{
+		return (-1);
+	}
 
-    my_jump = sqrt(size);
-    low = 0;
-    high = my_jump;
+	my_jump = sqrt(size);
+	low = 0;
+	high = my_jump;
 
-    while (low < size && array[low] <= value)
-    {
-        printf("Value checked array[%ld] = [%d]\n", low, array[low]);
+	while (low < size && array[low] <= value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", low, array[low]);
+		if (array[high] >= value || high >= size)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n", low, high);
+			for (i = low; i <= high && i < size; i++)
+			{
+				printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+				if (array[i] == value)
+				{
+					return (i);
+				}
+			}
+			return (-1);
+		}
+		low = high;
+		high += my_jump;
+		if (high > size - 1)
+		{
+			high = size;
+		}
+	}
 
-        if (array[high] >= value || high >= size)
-        {
-            printf("Value found between indexes [%ld] and [%ld]\n", low, high);
-            for (i = low; i <= high && i < size; i++)
-            {
-                printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-                if (array[i] == value)
-                {
-                    return (i);
-                }
-            }
-            return (-1);
-        }
-        low = high;
-        high += my_jump;
-        if (high > size - 1)
-        {
-            high = size;
-        }
-    }
-
-    return (-1);
+	return (-1);
 }
+
