e9105a9aba70ff758bbebccd77bd6d5b0eb17cef
antoineleno <johndoe@example.com>
2024-07-16 19:43:04 +0800

    search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,7 +14,7 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t low, high, my_jump, i;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 	{
 		return (-1);
 	}
---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,18 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * interpolation_search - Searching the value in a array using interpolation search
+ * @array: The array to search the value in
+ * @size: The size of the array
+ * @value: Value to be search in the array
+ * Return: The index of the value if found otherwise -1
+ */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+        if (array == NULL)
+        {
+                return (-1);
+        }
+}
